f79a3d8be6864af003b277e0ce8012b49061a764
lihaozhong <812419396@qq.com>
2021-11-21 08:34:44 +0800

    fix:修复密码没有加密

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
 }
