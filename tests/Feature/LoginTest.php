83e1e1525de3cde0872f71c72e87ee1e75602268
zero8coder <s812419396@gmail.com>
2023-06-15 17:10:17 +0800

    test:注册用户 密码登录

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -3,17 +3,19 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Cache;
 use DB;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
-    public function test_get_token_by_password()
+    // 密码方式获取token
+    public function test_login_by_password()
     {
-        $client = $this->createClient();
+        $client = $this->createPasswordClient();
         $user = User::factory()->create();
 
-        $response = $this->post('/oauth/token', [
+        $response = $this->post(route('api.v1.authorizations.store'), [
             'grant_type' => 'password',
             'client_id' => $client->id,
             'client_secret' => $client->secret,
@@ -21,12 +23,38 @@ class LoginTest extends TestCase
             'password' => 'password',
         ]);
 
-        $response->assertStatus(200);
+        $response->assertStatus(201);
         $response->assertJsonStructure([
             'token_type',
             'expires_in',
             'access_token',
             'refresh_token'
         ]);
+    }
+
+    // 图片验证码
+    public function test_pic_code()
+    {
+        $response = $this->post(route('api.v1.captchas.store'), ['phone' => '13160675344']);
+        $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'captcha_key',
+            'expired_at',
+            'captcha_image_content',
+        ]);
+    }
+
+    // 注册用户
+    public function test_register_user()
+    {
+        $response = $this->post(route('api.v1.captchas.store'), ['phone' => '13160675344']);
+        $data = $response->json();
+        $response = $this->post(route('api.v1.users.store'), [
+            'name' => 'king',
+            'password' => '123434',
+            'verification_key' => $data['captcha_key'],
+            'verification_code' => (Cache::get($data['captcha_key']))['code'],
+        ]);
+        $response->assertStatus(201);
     }
 }
