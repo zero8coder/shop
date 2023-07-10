b575601db2b3209824eaa5f783df8eb244d833c9
zero8coder <s812419396@gmail.com>
2023-07-10 10:44:38 +0800

    feature:保存权限

--- app/Http/Controllers/Admin/PermissionsController.php
+++ app/Http/Controllers/Admin/PermissionsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\PermissionRequest;
-use App\Http\Resources\PermissionResource;
+use App\Http\Resources\Admin\PermissionResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Spatie\Permission\Models\Permission;
--- tests/Feature/RoleTest.php
+++ tests/Feature/RoleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Database\Factories\PermissionFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -27,6 +28,32 @@ class RoleTest extends TestCase
                 'permissions'
             ]
         ]);
+    }
+
+    public function test_unauthorized_role_store()
+    {
+        $response = $this->json('POST', route('admin.v1.roles.store'));
+        $response->assertStatus(401);
+    }
+
+    public function test_role_store()
+    {
+        $this->signInAdmin();
+        $permission1 = (new PermissionFactory())->create(['name' => 'pp3']);
+        $permission2 = (new PermissionFactory())->create(['name' => 'pp4']);
+
+        $data = [
+            'name' => 'role1',
+            'permissions' => [
+                $permission1->id,
+                $permission2->id
+            ]
+        ];
+
+        $response = $this->json('POST', route('admin.v1.roles.store'), $data);
+        $response->assertStatus(200);
+        $response->assertSee('role1');
+        $this->assertDatabaseCount('role_has_permissions', 2);
     }
 
 }
